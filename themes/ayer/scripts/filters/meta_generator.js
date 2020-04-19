dc392d1b033e36120cb462b952f98e8dfe2f9c28
zhouzhihui <zhouzhihui@ruubypay.com>
2020-04-19 09:03:30 +0800

    commit ayer

--- themes/ayer/scripts/filters/meta_generator.js
+++ themes/ayer/scripts/filters/meta_generator.js
@@ -9,7 +9,7 @@ function hexoMetaGeneratorInject(data) {
     return;
   }
 
-  const hexoGeneratorTag = `\n  <meta name="generator" content="hexo-theme-ayer">`;
+  const hexoGeneratorTag = `\n  <meta name="generator" content="hexo-theme-yilia-plus">`;
 
   return data.replace('</title>', '</title>' + hexoGeneratorTag);
 }
